b9f54e6e0ac9f6d00e1bb80190aa697a8e2353d3
zhangyongxiang <zhangyongxiang@meituan.com>
2020-08-21 01:11:42 +0800

    111

---
+++ src/main/java/com/bestzyx/algorithms/binarytree/MinDepth_111.java
@@ -0,0 +1,33 @@
+package com.bestzyx.algorithms.binarytree;
+
+/**
+ * Created by zhangyongxiang(张永祥) on 2020/8/21 12:57 上午
+ * 给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例:
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9 20
+ * / \
+ * 15 7
+ * 返回它的最小深度  2.
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class MinDepth_111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left != null && root.right == null) {
+            return minDepth(root.left) + 1;
+        } else if (root.right != null && root.left == null) {
+            return minDepth(root.right) + 1;
+        } else {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+    
+}
