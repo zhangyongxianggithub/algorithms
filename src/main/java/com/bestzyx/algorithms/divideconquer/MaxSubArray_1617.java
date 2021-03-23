abed19e03afbf976b86c0e3e2f9618b9f11def6a
zhangyongxiang <zhangyongxiang@meituan.com>
2021-03-23 14:55:27 +0800

    update

--- src/main/java/com/bestzyx/algorithms/divideconquer/MaxSubArray_1617.java
+++ src/main/java/com/bestzyx/algorithms/divideconquer/MaxSubArray_1617.java
@@ -1,11 +1,26 @@
 package com.bestzyx.algorithms.divideconquer;
 
+import java.util.Arrays;
+
 /**
  * Created by zhangyongxiang(张永祥) on 2021/3/23 2:20 下午.
  **/
 public class MaxSubArray_1617 {
     
     public static int maxSubArray(int[] nums) {
+        int[] max = new int[nums.length];
+        Arrays.fill(max, 0);
+        max[0] = nums[0];
+        for (int index = 1; index < nums.length; index++) {
+            max[index] = Math.max(max[index - 1], max(nums, index));
+        }
+        return max[nums.length];
+    }
+    
+    private static int max(int[] mums, int end) {
+        for (; end >= 0; end--) {
+            
+        }
         return 0;
     }
 }
