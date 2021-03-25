cd20e1c67aa30cef97f7631c5f0c93aef6f0f45c
zhangyongxiang <1306835795@qq.com>
2021-03-25 20:17:35 +0800

    update

---
+++ src/main/java/com/bestzyx/algorithms/lecture/chapter3dp/Knapsack.java
@@ -0,0 +1,25 @@
+package com.bestzyx.algorithms.lecture.chapter3dp;
+
+/**
+ * @author zhangyongxiang
+ *         created on 2021/3/25 下午8:16.
+ **/
+public class Knapsack {
+    
+    public static void knapsack(final int[] v, final int[] w, final int c,
+            final int[][] m) {
+        final int n = v.length - 1;
+        final int jMax = Math.min(w[n], c);
+        for (int j = 0; j < jMax; j++) {
+            m[n][j] = 0;
+        }
+        for (int j = w[n]; j <= c; j++) {
+            m[n][j] = v[n];
+        }
+        for (int i = n - 1; i > 1; i--) {
+            // jMax=Math.min()
+        }
+        
+    }
+    
+}
