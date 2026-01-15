8cac6a57ae9fe6deae2fdf28d9d90a617adbf5db
zhangyongxiang <1306835795@qq.com>
2026-01-15 22:30:37 +0800

    update

---
+++ java/src/main/java/com/bestzyx/algorithms/java/Main.java
@@ -0,0 +1,12 @@
+package com.bestzyx.algorithms.java;
+
+/**
+ * Created by zhangyongxiang on 2026/1/15 22:28
+ *
+ * @author zhangyongxiang
+ */
+public class Main {
+    static void main() {
+        System.out.println("hello world");
+    }
+}
